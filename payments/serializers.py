7dbd79aee3c0967631361774019a3737e0375955
Kimbugwe Simon Peter <39520700+kimbugp@users.noreply.github.com>
2019-05-02 16:47:35 +0300

    - enable full amount payments only (#9)

--- payments/serializers.py
+++ payments/serializers.py
@@ -4,13 +4,23 @@ from utils.signals import invoice_date_generator
 
 
 class InvoicesSerializer(serializers.ModelSerializer):
+    amount_paid = serializers.FloatField()
+
     class Meta:
         model = Invoices
         exclude = ('updated_at', 'deleted_at', 'is_deleted',)
-        extra_kwargs = {'start_date': {
-            'read_only': True}, 'end_date': {'read_only': True}}
+        extra_kwargs = {'start_date': {'read_only': True},
+                        'end_date': {'read_only': True}}
 
     def create(self, validated_data):
         updated_data = invoice_date_generator(validated_data)
         instance = super().create(updated_data)
         return instance
+
+    def validate(self, data):
+        amount_paid = data.get('amount_paid')
+        rate = data.get('house').rate
+        if amount_paid % rate != 0:
+            raise serializers.ValidationError(
+                f'The amount paid should be in multiples of the house rate(UGX{rate})')  # noqa
+        return super().validate(data)
