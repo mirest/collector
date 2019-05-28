b14c7e6c532f002d80b1c1c797f49d54096f0e93
Kimbugwe Simon Peter <39520700+kimbugp@users.noreply.github.com>
2019-05-28 12:37:05 +0300

    Update views.py

--- houses/views.py
+++ houses/views.py
@@ -19,7 +19,7 @@ class HouseView(ListCreateAPIView):
     filter_backends = (DjangoFilterBackend, filters.SearchFilter, )
     permission_classes = (DjangoModelPermissions, IsAuthenticated)
     filter_class = HouseFilter
-    search_fields = ('tenant_id__name','house_name','rate',)
+    search_fields = ('tenant_id__name','house_name','rate','tenant_id__email')
 
 
 class SingleHouseView(RetrieveUpdateDestroyAPIView):
